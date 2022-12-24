161fa94735055a534f591e001daca2976b9b3a48
hyemib <hyem0127@gmail.com>
2022-12-25 01:39:43 +0900

    Initial Commit

---
+++ FirebasePractice/FirebasePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePracticeApp.swift
+//  FirebasePractice
+//
+//  Created by hyemi on 2023/01/07.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
